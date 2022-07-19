8b36a4007494d7e4e61c8f10ba1567617c301b9c
Rudresh-sisode <rudranrajput@gmail.com>
2022-07-19 20:11:45 +0530

    digit root else code added

--- Javascript/digit_root.js
+++ Javascript/digit_root.js
@@ -23,3 +23,11 @@ function digital_root(n) {
     
     return Number(str);
   }
+
+
+  function digital_root(n) {
+    if (n < 10) return n;
+    
+    return digital_root(
+      n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
+  }
