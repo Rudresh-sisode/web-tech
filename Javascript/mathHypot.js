0832506e71d673d2029b1f2523759047bd8f299e
Rudresh-sisode <rudranrajput@gmail.com>
2022-07-22 11:45:34 +0530

    math hypot functions

---
+++ Javascript/mathHypot.js
@@ -0,0 +1,17 @@
+console.log(Math.hypot(3, 4));
+// expected output: 5
+
+console.log(Math.hypot(5, 12));
+// expected output: 13
+
+console.log(Math.hypot(3, 4, 5));
+// expected output: 7.0710678118654755
+
+console.log(Math.hypot(-5));
+
+/**
+ *  The Math.hypot() function returns the square root of the sum of squares of its arguments, that is:
+
+Math.hypot ( v 1 , v 2 , … , v n ) = ∑ i = 1 n v i 2 = v 1 2 + v 2 2 + … + v n 2 \mathtt{\operatorname{Math.hypot}(v
+1, v_2, \dots, v_n)} = \sqrt{\sum{i=1}^n v_i^2} = \sqrt{v_1^2 + v_2^2 + \dots + v_n^2} 
+ */
