fef1124d3a71d672965c7d65e6ec51cb039342cc
Rudresh-sisode <rudranrajput@gmail.com>
2022-07-22 11:50:22 +0530

    new prob

---
+++ Javascript/unSolvedProblem.js
@@ -0,0 +1,27 @@
+// This kata is inspired by This Kata
+
+// We have a string s
+
+// We have a number n
+
+// Here is a function that takes your string, concatenates the even-indexed chars to the front, odd-indexed chars to the back.
+// Examples
+
+// s = "Wow Example!"
+// result = "WwEapeo xml!"
+// s = "I'm JomoPipi"
+// result = "ImJm ii' ooPp"
+
+// The Task:
+
+// return the result of the string after applying the function to it n times.
+
+// example where s = "qwertyuio" and n = 2:
+
+// after 1 iteration  s = "qetuowryi"
+// after 2 iterations s = "qtorieuwy"
+// return "qtorieuwy"
+
+// Note
+
+// there's a lot of tests, big strings, and n is greater than a billion
