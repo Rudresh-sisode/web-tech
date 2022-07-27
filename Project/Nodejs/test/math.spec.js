9053acd7039ec32efd2079447ee5044d844e0cd2
Rudresh-sisode <rudranrajput@gmail.com>
2022-07-27 12:11:04 +0530

    AAA pattern

--- Project/Nodejs/test/math.spec.js
+++ Project/Nodejs/test/math.spec.js
@@ -5,3 +5,23 @@ it('should sumerized all number',()=>{
     const result = add([3,3])
     expect(result).to.toBe(6);
 })
+
+it('yield if NaN',()=>{
+    const input = ['in',1];
+    const result = add(input)
+
+    expect(result).toBeNaN();
+})
+
+//AAA Pattern
+it('summer all value',()=>{
+    //Arrange
+    const number = [2,3,4];
+
+    //Act
+    const result = add(number);
+
+    //Assert
+    const expResult = number.reduce((pr,cur)=> pr + cur,0);
+    expect(result).toBe(expResult);
+})
