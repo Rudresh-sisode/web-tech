7109f32ec0f3c975152fa4a4c1257282474f58b5
Rudresh-sisode <rudranrajput@gmail.com>
2022-08-04 15:06:34 +0530

    cart logic added

--- ReactJs/advanceRedux-App-Demo/src/components/Cart/CartButton.js
+++ ReactJs/advanceRedux-App-Demo/src/components/Cart/CartButton.js
@@ -1,12 +1,15 @@
+import { uiActions } from '../../store/ui-slice';
+import { useDispatch } from 'react-redux';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
   const toggleCartHandler = ()=>{
+    dispatch(uiActions.toggle());
+  } 
 
-  }
-  
   return (
-    <button className={classes.button} onClick={}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
