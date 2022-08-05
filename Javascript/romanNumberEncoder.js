65c3909d3931302f43df2d448916d4d21a360fba
Rudresh-sisode <rudranrajput@gmail.com>
2022-08-05 17:52:29 +0530

    Two approch

--- Javascript/romanNumberEncoder.js
+++ Javascript/romanNumberEncoder.js
@@ -23,3 +23,29 @@ Example:
 solution(1000); // should return 'M'
 
  */
+
+//approched second
+
+function solution(number){
+    const table = [
+      [1000,'M'],
+      [900,'CM'],
+      [500,'D'],
+      [400,'CD'],
+      [100,'C'],
+      [90,'XC'],
+      [50,'L'],
+      [40,'XL'],
+      [10,'X'],
+      [9,'IX'],
+      [5,'V'],
+      [4,'IV'],
+      [1,'I']
+    ];
+    
+    for (let [num, notation] of table) {
+      if (number >= num)
+        return notation + solution(number - num)
+    }
+    return ''
+  }
