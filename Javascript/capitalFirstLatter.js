26a9902e751870335c01ea8527218a39dfd0c8a4
Rudresh-sisode <rudranrajput@gmail.com>
2022-08-08 16:44:12 +0530

    access token and refresh token added

---
+++ Javascript/capitalFirstLatter.js
@@ -0,0 +1,5 @@
+function firstCapital(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+console.log(firstCapital("sldkjflskj"))
---
+++ NodeJs/refres-access-tkn/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+app.use(express.json());
+
+function auth(req,res,next){
+    let token = req.headers['authorization'];
+    token = token.split(' ')[1];//access token
+    console.log("lsls") 
+    jwt.verify(token,"aCceSs",(err,user)=>{
+        if(!err){
+            req.user = user;
+            next();
+        }
+        else{
+            return res.status(401).json({
+                message:"Not allow"
+            })
+        }
+       
+    })
+
+    //
+}
+app.post('/protected',auth, (req,res,next)=>{
+    res.send("inside protected route")
+})
+app.post('/login',(req,res,next)=>{
+    
+    const user= req.body.user
+
+    if(!user){
+        res.status(404).json({
+            message:"body is empty~"
+        })
+    }
+
+    let accessToken = jwt.sign(user,'aCceSs',{expiresIn:'2m'});
+    let refreshToken = jwt.sign(user,"rEfResH",{expiresIn:'1m'});
+
+    return res.status(201).json({
+        accessToken,
+        refreshToken
+    })
+})
+
+console.log("on port ~3000")
+app.listen(3000);
