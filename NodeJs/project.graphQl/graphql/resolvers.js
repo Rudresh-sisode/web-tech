62c2f9c35ed04e52e9f6042bece92113c312e414
Rudresh-sisode <rudranrajput@gmail.com>
2022-08-09 19:50:02 +0530

    mongodb connection done, add data

--- NodeJs/project.graphQl/graphql/resolvers.js
+++ NodeJs/project.graphQl/graphql/resolvers.js
@@ -1,8 +1,34 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
 module.exports = {
-    hello:()=>{
-        return {
-            text:'~Hello word~',
-            views:123
+    createUser:async function({userInput},req){
+        // const email = args.userInput.email;
+        const existingUser = await User.findOne({email:userInput.email}).lean()
+        if(existingUser){
+            const error = new Error("User exists already!");
+            throw error;
         }
+        const hashedPw = await bcrypt.hash(userInput.password,12);
+
+        const user = new User({
+            email:userInput.email,
+            name:userInput.name,
+            password:hashedPw
+        });
+
+        const createdUser = await user.save();
+        return {...createdUser._doc, _id: createdUser._id.toString()}
     }
 }
+
+
+
+// module.exports = {
+//     hello:()=>{
+//         return {
+//             text:'~Hello word~',
+//             views:123
+//         }
+//     }
+// }
