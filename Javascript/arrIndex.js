bb5148123f3fa997cc4b7c0e501fa98a0f26371f
Rudresh-sisode <rudranrajput@gmail.com>
2022-08-12 10:57:21 +0530

    concat array with first index

---
+++ Javascript/arrIndex.js
@@ -0,0 +1,17 @@
+function arrAdder(arr) {
+    var sentence = "";
+    for(var i = 0; i < arr[0].length; i++){
+      for(var j = 0; j < arr.length; j++){
+        sentence+=arr[j][i];
+      }
+      sentence+=" ";
+    }
+    return sentence.trim();
+  }
+
+  console.log(arrAdder([
+    ['J','L','L','M'],
+    ['u','i','i','a'],
+    ['s','v','f','n'],
+    ['t','e','e','']
+    ]))
