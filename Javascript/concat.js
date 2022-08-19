6b766289404847b0b5fb2ea5d5eaaf627366ea57
Rudresh-sisode <rudranrajput@gmail.com>
2022-08-19 23:15:11 +0530

    array concat with shallo copy

---
+++ Javascript/concat.js
@@ -0,0 +1,13 @@
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+
+console.log(array3);
+// expected output: Array ["a", "b", "c", "d", "e", "f"]
+
+const letters = ['a', 'b', 'c'];
+const numbers = [1, 2, 3];
+
+const alphaNumeric = letters.concat(numbers);
+console.log(alphaNumeric);
+// results in ['a', 'b', 'c', 1, 2, 3]
