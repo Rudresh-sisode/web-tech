3baff60cb4ca359856ebbdfcc1d5187d53744e9a
Rudresh-sisode <rudranrajput@gmail.com>
2022-08-20 16:17:51 +0530

    reactive from testing | Angular

---
+++ Angular/reactive.form-component.spec.ts
@@ -0,0 +1,21 @@
+//testing the reactive form model to view
+
+it('should update the value in the control', () => {
+    component.favoriteColorControl.setValue('Blue');
+  
+    const input = fixture.nativeElement.querySelector('input');
+  
+    expect(input.value).toBe('Blue');
+  });
+
+  // view to model
+
+  it('should update the value of the input field', () => {
+    const input = fixture.nativeElement.querySelector('input');
+    const event = createNewEvent('input');
+  
+    input.value = 'Red';
+    input.dispatchEvent(event);
+  
+    expect(fixture.componentInstance.favoriteColorControl.value).toEqual('Red');
+  });
