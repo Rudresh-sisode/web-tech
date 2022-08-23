a008f169a9921c55fc142530c83b3995b646eb98
Rudresh-sisode <rudranrajput@gmail.com>
2022-08-23 19:50:41 +0530

    entries example | javascript

---
+++ Javascript/entries.javascript.js
@@ -0,0 +1,28 @@
+const array = ["a", "b", "c"];
+const arrayEntries = array.entries();
+
+for (const element of arrayEntries) {
+  console.log(element);
+}
+
+
+const a = ["a", "b", "c"];
+
+for (const [index, element] of a.entries()) {
+  console.log(index, element);
+}
+
+// 0 'a'
+// 1 'b'
+// 2 'c'
+
+
+const array1 = ['a', 'b', 'c'];
+
+const iterator1 = array1.entries();
+
+console.log(iterator1.next().value);
+// expected output: Array [0, "a"]
+
+console.log(iterator1.next().value);
+// expected output: Array [1, "b"]
