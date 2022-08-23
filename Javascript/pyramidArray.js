7ae3f8387d2ccf073e0a53b6cf62a516f728650e
Rudresh-sisode <rudranrajput@gmail.com>
2022-08-23 11:27:40 +0530

    Javascript | Pyramid Array

---
+++ Javascript/pyramidArray.js
@@ -0,0 +1,22 @@
+function pyramid(n) {
+    // your code here
+    const Acontainer = [];
+    for(let i = 0; i < n ; i++){
+      const Aelement = new Array(1+i);
+      for(let j = 0; j <= i; j++){
+        Aelement[j] = 1;
+      }
+      Acontainer.push(Aelement)
+    }
+    return Acontainer;
+  }
+
+  //Approched 2
+
+  function pyramid(n) {
+    const res = [];
+    for(let i = 0; i < n; i++){
+      res.push([...Array(i+1)].fill(1))
+    }
+    return res;
+  }
