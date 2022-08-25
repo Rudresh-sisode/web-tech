7381a7dfc83212f130e30515aae610c0092c19a7
Rudresh-sisode <rudranrajput@gmail.com>
2022-08-25 19:37:28 +0530

    odd even | 3 Approched

--- Javascript/oddEvenString.js
+++ Javascript/oddEvenString.js
@@ -12,14 +12,14 @@ function sortMyString(S) {
   console.log("your abc ",abc)
 
 
-  /**
-   * #Approch 2
-   * const sortMyString = s => {
+  
+   // #Approch 2
+    const sortMyString = s => {
     let even = s.split('').filter((v, i) => i % 2 === 0).join('')
     let odd = s.split('').filter((v, i) => i % 2 !== 0).join('')
     return even + ' ' + odd
    } 
-   */
+   
 
     /**
      * approched 3
