208a3f7db3148a79db3acc42de7c47653101f720
Rudresh-sisode <rudranrajput@gmail.com>
2022-08-31 15:45:01 +0530

    array every | aprroched 2

--- Javascript/array-every.js
+++ Javascript/array-every.js
@@ -2,3 +2,9 @@ const isBelow = (currentValue)=> currentValue < 10;
 
 let array = [2,3,4,5,6,77,9];
 console.log(array.every(isBelow));
+
+
+// second example
+
+[12, 5, 8, 130, 44].every((x) => x >= 10);   // false
+[12, 54, 18, 130, 44].every((x) => x >= 10); // true
