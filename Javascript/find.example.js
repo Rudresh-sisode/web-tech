ef33db511f03b667f0748021467545dddc8c7483
Rudresh-sisode <rudranrajput@gmail.com>
2022-10-04 19:54:34 +0530

    find | method | syntax

---
+++ Javascript/find.example.js
@@ -0,0 +1,13 @@
+find((element) => { /* … */ } )
+find((element, index) => { /* … */ } )
+find((element, index, array) => { /* … */ } )
+
+// Callback function
+find(callbackFn)
+find(callbackFn, thisArg)
+
+// Inline callback function
+find(function(element) { /* … */ })
+find(function(element, index) { /* … */ })
+find(function(element, index, array){ /* … */ })
+find(function(element, index, array) { /* … */ }, thisArg)
