77b0b2d0c849c4e303e99ad62e20b68336d14d86
Rudresh-sisode <rudranrajput@gmail.com>
2022-12-07 19:09:55 +0530

    configurinig of handling query params

--- Angular/Router/router-parameter-reactively.ts
+++ Angular/Router/router-parameter-reactively.ts
@@ -1,3 +1,16 @@
 export class UserComponent implements OnInit{
 
+    ngOnInit(){
+        const id = +this.route.snapshot.params['id'];
+        this.server = this.serverService.getServer(id);
+        this.route.params.subscribe(params:Params)=>{
+            this.server = this.serversService.getServer(+params['id']);
+        }
+
+    }
+
+    onEdit(){
+        this.router.navigate(['edit'],{relativeTo:this.route,queryParamsHandling:'preserve'})
+    }
+
 }
