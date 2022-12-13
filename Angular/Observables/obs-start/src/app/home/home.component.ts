aecb4a73c5650333fcb14ab73a3dc4764f234211
Rudresh-sisode <rudranrajput@gmail.com>
2022-12-13 16:50:55 +0530

    custome observable

--- Angular/Observables/obs-start/src/app/home/home.component.ts
+++ Angular/Observables/obs-start/src/app/home/home.component.ts
@@ -10,6 +10,18 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const customeIntervalObserable = Observable.create(observable=>{
+      let count = 0;
+
+      setInterval(()=>{
+        observable.next(count)
+        count++;
+      },1000)
+    })
   }
+
+  customeIntervalObserable.subscribe(data=>{
+    console.log('your data ',data);
+  })
 
 }
