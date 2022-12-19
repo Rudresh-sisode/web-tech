4adda5e07713d8ae1ffa932ebd99b66cb568b841
Rudresh-sisode <rudranrajput@gmail.com>
2022-12-19 11:09:36 +0530

    Subject | Angular

--- Angular/Observables/obs-start/src/app/app.component.ts
+++ Angular/Observables/obs-start/src/app/app.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  private activatedSub:Subscription;
+  constructor(private  userService:UserService) {}
 
   ngOnInit() {}
+
+  this.activatedSub = this.userService.activatedEmmitSubject.subscribe(didActive=>{
+    this.userActive = didActive
+  })
+
+  ngOnDestroy(){
+    
+  }
 }
