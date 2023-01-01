ea341dcd9ab46f7248ba8be270ae0e98b885b4bb
Rudresh-sisode <rudranrajput@gmail.com>
2023-01-01 14:33:03 +0530

    add custome validator

--- Angular/Dec-Angular-Lectureship/forms-reactive-start/src/app/app.component.ts
+++ Angular/Dec-Angular-Lectureship/forms-reactive-start/src/app/app.component.ts
@@ -9,11 +9,12 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm : FormGroup;
+  forbiddenUsernames = ['Chirs','Ana'];
 
   ngOnInit(){
     this.signupForm = new FormGroup({
       'userData':new FormGroup({
-        'username':new FormControl(null,Validators.required),
+        'username':new FormControl(null,[Validators.required,this.forbiddenNames.bind(this)]),
         'email':new FormControl(null,[Validators.required, Validators.email]),
       }),
       'gender':new FormControl('male'),
@@ -37,4 +38,13 @@ export class AppComponent implements OnInit{
   onSubmit(){
     console.log(this.signupForm);
   }
+
+  forbiddenNames(control:FormControl):{[s:string]:boolean /** {'abc':true} */}{
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden':true}
+    }
+    return null;
+    //if validation fail, you only return null rather not return false or anyother values
+  }
+
 }
