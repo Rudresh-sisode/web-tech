beb44ae8e761c2196101484cb3829aa6198959b1
Rudresh-sisode <rudranrajput@gmail.com>
2023-01-02 19:38:00 +0530

    flutter app enhance| new changes apply

---
+++ Flutter/projects/perso_exp_enhance-6/lib/widgets/chart.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+import './chart_bar.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+
+   Chart(this.recentTransaction){
+    print("Contructor Chart ");
+  }
+
+  List<Map<String, Object>> get groupedTransactionValud {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double totalSum = 0.0;
+      for (var i = 0; i < recentTransaction.length; i++) {
+        if (recentTransaction[i].date.day == weekDay.day &&
+            recentTransaction[i].date.month == weekDay.month &&
+            recentTransaction[i].date.year == weekDay.year) {
+          totalSum += recentTransaction[i].amount;
+        }
+      }
+
+      print(DateFormat.E().format(weekDay));
+      print(totalSum);
+
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    }).reversed.toList();
+  }
+
+  double get maxSpending {
+    return groupedTransactionValud.fold(0.0, (previousValue, element) {
+      return previousValue + (element['amount'] as double);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("Build() Chart");
+    return  Card(
+        elevation: 6,
+        margin: EdgeInsets.all(20),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: groupedTransactionValud.map((data) {
+                // return Text('${data['day']} : ${data['amount']}');
+                return Flexible(
+                  fit: FlexFit.tight,
+                    child: ChartBar(
+                        data['day'],
+                        data['amount'],
+                        maxSpending == 0
+                            ? 0.0
+                            : (data['amount'] as double) / maxSpending));
+              }).toList()),
+        ),
+      );
+  }
+}
---
+++ Flutter/projects/perso_exp_enhance-6/lib/widgets/new_transaction.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final _titleController = TextEditingController();
+
+  final _amountController = TextEditingController();
+
+  DateTime _selectedDate;
+
+  double amountValue;
+
+  void _submitData() {
+    print("button press");
+    if (_amountController.text.isEmpty || _titleController.text.isEmpty) {
+      return;
+    }
+
+    final enteredTtile = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
+
+    if (enteredTtile.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
+      return;
+    }
+
+    widget.addTx(enteredTtile, enteredAmount, _selectedDate);
+    Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2022),
+            lastDate: DateTime.now())
+        .then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        
+        child: Container(
+          // padding: const EdgeInsets.all(10),
+          padding: EdgeInsets.only(top: 10, left: 10, right: 10, bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                ) /**onChanged: (value){ titleInput = value; } */,
+                controller: _titleController,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Amount',
+                ) /**,onChanged: (value){ amountInput = value;} */,
+                controller: _amountController,
+                onSubmitted: (_) => _submitData(),
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(_selectedDate == null
+                            ? "No Date Choosen"
+                            : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}')),
+                    TextButton(
+                        // style:ButtonStyle(foregroundColor: Theme.of(context).primaryColor) ,//
+                        onPressed: _presentDatePicker,
+                        child: Text(
+                          "Choose Date",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ))
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _submitData,
+                child: Text("Add Transaction"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
