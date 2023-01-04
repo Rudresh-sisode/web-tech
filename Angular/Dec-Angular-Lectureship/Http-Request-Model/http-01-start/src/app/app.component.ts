b72eae84d5f2db7320f7cd2bbf0d5a862efbdbb5
Rudresh-sisode <rudranrajput@gmail.com>
2023-01-04 11:16:55 +0530

    rxjs operator and get and post request

---
+++ Angular/Dec-Angular-Lectureship/Http-Request-Model/http-01-start/src/app/app.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+import {Post} from './post.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchPosts();
+   }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
+    this.http.post('https://myfirst-242709-default-rtdb.firebaseio.com/posts.json', postData)
+    .subscribe(
+      {
+        next: (response) =>{
+          console.log("response from backend ",response);
+        },
+        error: (error)=>{
+          console.log("your error occured here ",error);
+        },
+        complete: ()=>{
+          //pass done.
+        }
+      }
+    )
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+
+  }
+
+  private fetchPosts(){
+    this.http.get<{[key:string]:Post}>('https://myfirst-242709-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map((responseData : {[key:string]:Post}) =>{
+      const postArray:Post[] = [];
+      for(const key in responseData){
+        if(responseData.hasOwnProperty(key)){
+          postArray.push({...responseData[key], id:key});
+        }
+      }
+      return postArray;
+    }))
+    .subscribe(
+      {
+        next:(response)=>{
+          console.log('get response ',response);
+        },
+        error:(error)=>{
+
+        },
+        complete:()=>{
+
+        }
+      }
+    )
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
---
+++ Angular/Dec-Angular-Lectureship/Http-Request-Model/http-01-start/src/app/post.model.ts
@@ -0,0 +1,5 @@
+export interface Post{
+    titile:string,
+    content:string,
+    id?:string
+}
