2f9141188eef5983d0d54d02e5cc97e99623c01c
Rudresh-sisode <rudranrajput@gmail.com>
2023-01-11 23:54:37 +0530

    Authentication in Angular | Implementing

--- Angular/Dec-Angular-Lectureship/Auth/src/app/auth/auth.service.ts
+++ Angular/Dec-Angular-Lectureship/Auth/src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 interface AuthResponseData{
     kind:string,
@@ -7,7 +9,8 @@ interface AuthResponseData{
     email:string,
     refreshToken:string,
     expiresIn:string,
-    localId:string
+    localId:string,
+    registered?:boolean;
 }
 
 
@@ -23,6 +26,25 @@ export class AuthService{
         email:email,
         password:password,
         returnSecureToken:true
+        })
+        .pipe(catchError((errorRes:any) =>{
+            let errorMessage = 'An unknown error occured'
+            if(!errorRes.error || !errorRes.error.error){
+                return throwError(errorMessage);
+            }
+            switch(errorRes.error.error.message){
+                case 'EMAIL_EXISTS':
+                    errorMessage = 'This email exists already';
+            }
+            return throwError(errorMessage);
+        }));
+    }
+
+    login(email:string,password:string){
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDvGuH81wN-4PIgOTWZ7BeOuCGKLG5rzVw',{
+            email:email,
+            password:password,
+            returnSecureToken:true
         })
     }
 }
