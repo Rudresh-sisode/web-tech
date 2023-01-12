a77588c5032679f75ec4b78c7a3b0b770c31fafb
Rudresh-sisode <rudranrajput@gmail.com>
2023-01-12 10:56:00 +0530

    auth error handling logic build

--- Angular/Dec-Angular-Lectureship/Auth/src/app/auth/auth.service.ts
+++ Angular/Dec-Angular-Lectureship/Auth/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
@@ -27,17 +27,7 @@ export class AuthService{
         password:password,
         returnSecureToken:true
         })
-        .pipe(catchError((errorRes:any) =>{
-            let errorMessage = 'An unknown error occured'
-            if(!errorRes.error || !errorRes.error.error){
-                return throwError(errorMessage);
-            }
-            switch(errorRes.error.error.message){
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'This email exists already';
-            }
-            return throwError(errorMessage);
-        }));
+        .pipe(catchError(this.handleError));
     }
 
     login(email:string,password:string){
@@ -45,6 +35,25 @@ export class AuthService{
             email:email,
             password:password,
             returnSecureToken:true
-        })
+        }).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes:HttpErrorResponse){
+        let errorMessage = 'An unknown error occured'
+            if(!errorRes.error || !errorRes.error.error){
+                return throwError(errorMessage);
+            }
+            switch(errorRes.error.error.message){
+                case 'EMAIL_EXISTS':
+                    errorMessage = 'This email exists already';
+                    break;
+                case 'EMAIL_NOT_FOUND':
+                    errorMessage = 'This email does not exist';
+                    break;
+                case 'INVALID_PASSWORD':
+                    errorMessage = 'This password is not correct';
+                    break;
+            }
+            return throwError(errorMessage);
     }
 }
