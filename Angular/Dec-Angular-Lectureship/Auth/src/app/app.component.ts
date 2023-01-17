e255cf2a29163c7d9fdec66a3df60159802b2b62
Rudresh-sisode <rudranrajput@gmail.com>
2023-01-17 10:51:57 +0530

    Auto logout and auto login done

--- Angular/Dec-Angular-Lectureship/Auth/src/app/app.component.ts
+++ Angular/Dec-Angular-Lectureship/Auth/src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  constructor(private authService:AuthService){}
+
+  ngOnInit(){
+    this.authService.autoLogin();
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
--- Angular/Dec-Angular-Lectureship/Auth/src/app/auth/auth.service.ts
+++ Angular/Dec-Angular-Lectureship/Auth/src/app/auth/auth.service.ts
@@ -21,6 +21,7 @@ export class AuthService{
 
     user = new BehaviorSubject<User>(null);
     token : string = null;
+    private tokenExpirationTimer:any;
 
     constructor(private http:HttpClient, private router:Router){
 
@@ -40,6 +41,19 @@ export class AuthService{
     logout(){
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if(this.tokenExpirationTimer){
+            clearTimeout(this.tokenExpirationTimer);
+        }
+        this.tokenExpirationTimer = null;
+    }
+
+    autoLogout(expirationDuration){
+
+        this.tokenExpirationTimer =setTimeout(()=>{
+            this.logout();
+        },expirationDuration);
+
     }
 
     login(email:string,password:string){
@@ -57,9 +71,33 @@ export class AuthService{
         const expirationDate  = new Date(new Date().getTime() + expiresIn * 1000);
             const user = new User(email,userId,token,expirationDate);
             this.user.next(user);
+            this.autoLogout(expirationDate)
             localStorage.setItem('userData',JSON.stringify(user));
 
     }
+
+    autoLogin(){
+        const userData:{
+            email:string;
+            id:string;
+            _token:string;
+            _tokenExpirationDate:string;
+        } = JSON.parse(localStorage.getItem('userData'));
+
+        if(!userData){
+            return;
+        }
+
+        const loadedUser = new User(userData.email,userData.id,userData._token,new Date(userData._tokenExpirationDate));
+
+        if(loadedUser.token){
+            this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+            this.autoLogout(expirationDuration);
+        }
+    }
+
+    
 
     private handleError(errorRes:HttpErrorResponse){
         let errorMessage = 'An unknown error occured'
