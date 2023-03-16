cb9567666e5973ee15d231456b76582ccde97d93
Rudresh-sisode <rudranrajput@gmail.com>
2023-03-16 09:26:03 +0530

    action and reducer added for shopping list

---
+++ Angular/Dec-Angular-Lectureship/Auth/src/app/shopping-list/store/shopping-list.actions.ts
@@ -0,0 +1,13 @@
+import {Action} from '@ngrx/store';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+export const START_EDIT = 'START_EDIT';
+export const STOP_EDIT  = 'STOP_EDIT';
+
+export class AddIngredient implements Action{
+    readonly type= ADD_INGREDIENT;
+}
