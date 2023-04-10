31ae0730774372d77e3efc4bf7ae775f385db619
Rudresh-sisode <rudranrajput@gmail.com>
2023-04-10 11:14:50 +0530

    parsing http paths

--- NodeJs/pirple.com/app/index.js
+++ NodeJs/pirple.com/app/index.js
@@ -4,10 +4,22 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
+    // Get the URL and parse it
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
+    res.end('Hello World\n');
+
+    // Log the request path
+    console.log('Request received on path: ' + trimmedPath);
 });
 
 // Start the server, and have it listen on port 3000
