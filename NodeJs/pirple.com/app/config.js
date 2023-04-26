fa29bc74945cbe34a805240cb14ddba718b316fc
Rudresh-sisode <rudranrajput@gmail.com>
2023-04-26 11:06:09 +0530

    added the twilio code, credential still on the line

--- NodeJs/pirple.com/app/config.js
+++ NodeJs/pirple.com/app/config.js
@@ -11,7 +11,12 @@ environments.staging = {
     'httpsPort' : 3001,
     'envName' : 'staging',
     'hashingSecret':'thisIsASecret',
-    'maxChecks': 5
+    'maxChecks': 5,
+    'twilio' : {
+        'accountSid':'',
+        'authToken':'',
+        'fromPhone':''
+    }
 
 };
 
@@ -21,7 +26,12 @@ environments.production = {
     'httpsPort' : 5001,
     'envName' : 'production',
     'hashingSecret':'thisIsASecret',
-    'maxChecks': 5
+    'maxChecks': 5,
+    'twilio' : {
+        'accountSid':'',
+        'authToken':'',
+        'fromPhone':''
+    }
 
 };
 
