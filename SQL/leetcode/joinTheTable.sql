8d90572c899b729a71934ea5194d705bf30c0702
Rudresh-sisode <rudranrajput@gmail.com>
2023-07-09 15:57:45 +0530

    inner join on multiple tables

--- SQL/leetcode/joinTheTable.sql
+++ SQL/leetcode/joinTheTable.sql
@@ -1,2 +1,8 @@
+-- inner join query
 select u.email, e.first_name, e.last_name 
 from employee_tables as e inner join user_tables as u on u.id = e.user_id;
+
+--  inner join on multiple tables
+select u.email, e.first_name, e.last_name, att.date, att.check_in_time as attenTime
+from employee_tables as e inner join user_tables as u on u.id = e.user_id
+inner join attendance_tables as att on e.id = att.employee_id;
