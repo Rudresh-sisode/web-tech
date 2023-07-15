7836725664b3d55f902a8dfcdde40d8f5dfdaa0d
Rudresh-sisode <rudranrajput@gmail.com>
2023-07-15 15:57:40 +0530

    email dubplicate return

--- SQL/reportDublicateemail.sql
+++ SQL/reportDublicateemail.sql
@@ -1,1 +1,2 @@
 -- Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+select email AS Email from Person GROUP BY email HAVING COUNT(*) > 1
