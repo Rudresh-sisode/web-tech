4db325e07cb8270761dca3d9f6335b6404c1b2e8
Rudresh-sisode <rudranrajput@gmail.com>
2023-09-04 08:33:48 +0530

    add two sum solution optimized

--- Javascript/minimumReplacementToSortTheArray.js
+++ Javascript/minimumReplacementToSortTheArray.js
@@ -20,3 +20,30 @@ var minimumReplacement = function(nums) {
 };
 
 console.log(minimumReplacement([7,3,5,4,8]))
+
+
+// Second approched
+
+var minimumReplacement = function(nums) {
+  let currentLargest = nums[nums.length - 1];
+  let totalReplacements = 0;
+  
+  for (let i = nums.length - 1; i >= 0; i--) {
+      if (nums[i] <= currentLargest) {
+          currentLargest = nums[i];
+          continue;
+      }
+
+      let numElements;
+      if (nums[i] % currentLargest) {
+          numElements = Math.floor(nums[i] / currentLargest) + 1;
+          currentLargest = Math.floor(nums[i] / numElements);
+      } else {
+          numElements = Math.floor(nums[i] / currentLargest);
+      }
+      
+      totalReplacements += numElements - 1;
+  }
+  
+  return totalReplacements;    
+};
--- Javascript/twoSum.js
+++ Javascript/twoSum.js
@@ -1,10 +1,22 @@
-var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++){
- for(let j= i+1; j < nums.length; j++){
- if(nums[i] + nums[j] == target){
- return [i,j];
-//  break;
-}
- }
- }
-};
+// var twoSum = function(nums, target) {
+//     for(let i = 0; i < nums.length; i++){
+//  for(let j= i+1; j < nums.length; j++){
+//  if(nums[i] + nums[j] == target){
+//  return [i,j];
+// //  break;
+// }
+//  }
+//  }
+// };
+
+const twoSum = (nums, target) => {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+      if (map.has(complement)) {
+        return [map.get(complement), i];
+      }
+      map.set(nums[i], i);
+    }
+  };
+    console.log(twoSum([2,7,11,15],9))
