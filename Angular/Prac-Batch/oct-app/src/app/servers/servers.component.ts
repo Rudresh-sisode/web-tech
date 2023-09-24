c7c4aaa9e4dda0a0bebfcc98b96e740e2ab6c228
Rudresh-sisode <rudranrajput@gmail.com>
2023-09-24 17:53:56 +0530

    property binding and string interpolation

--- Angular/Prac-Batch/oct-app/src/app/servers/servers.component.ts
+++ Angular/Prac-Batch/oct-app/src/app/servers/servers.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '.app-servers',
-  // templateUrl: './servers.component.html',
-  template:`<app-server></app-server>
-            <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+ 
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
 }
