a44a7c47d090f02c0c156e80c41fc27efd2f888f
Rudresh-sisode <rudranrajput@gmail.com>
2023-10-02 16:13:43 +0530

    can't create a instance of abstract class

---
+++ TypeScript/classes/app.ts
@@ -0,0 +1,19 @@
+abstract class FirstBase{
+    constructor(public name:string){
+        this.name = name;
+    }
+
+    abstract getName():string;
+}
+
+class Child extends FirstBase{
+    constructor(public name:string){
+        super(name);
+    }
+
+    getName(): string {
+        return this.name;
+    }
+}
+
+let instanceOfClass = new Child("");
