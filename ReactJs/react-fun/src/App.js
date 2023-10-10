5f9a7c18162c351de2f7058bc260957200d99738
Rudresh-sisode <rudranrajput@gmail.com>
2023-10-11 00:09:03 +0530

    working with render probs

--- ReactJs/react-fun/src/App.js
+++ ReactJs/react-fun/src/App.js
@@ -2,42 +2,84 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect, useReducer, useRef } from 'react';
 
+// const query = `
+// query {
+//   allLifts {
+//     name,
+//     elevationGain,
+//     status
+//   }
+// }
+// `
 
-function GithubUser({name}){
-  return (
-    <div>
-      <h2>
-        {name} is learning React
-      </h2>
-    </div>
+// const opts = {
+//   method:"POST",
+//   headers:{ "Content-Type":"application/json"},
+//   body:JSON.stringify({query})
+// }
+
+// function GithubUser({name,elevationGain, status}){
+//   return (
+//     <div>
+//       <h2>
+//         {name} is learning React
+//       </h2>
+//       <p>
+//         {elevationGain} {status}
+//       </p>
+//       {/* <img src={} height={150} alt='avatar'/> */}
+//     </div>
+//   )
+// }
+
+const dataLs = [{
+    name:"asl-tera", elevation:45839
+  },
+  {
+    name:"mount-everest",elevation:494884
+  },
+  {
+    name:"Juna-gad", elevation:4599
+  },
+  {
+    name:"Neah-Ghat", elevation:9048
+  }
+  ]
+
+function List({data,renderItem,renderEmpty}){
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {
+        data.map((item)=>(
+          <li key={item.name}>
+            {renderItem(item)}
+          </li>
+        ))
+      }
+    </ul>
   )
 }
 
 function App({state}) {
  
-  const [data,setData] = useState(null);
-  const [error,setError] = useState(null);
-  const [loading,setLoading] = useState(false);
+return (
+        <List 
+        data = {dataLs} 
+        renderItem={(item)=>(
+            <>
+            {item.name} - {item.elevation} ft.
+            </>
+          )
+        }
+        renderEmpty={<p>No content available as of now!</p>}
+  />
+  )
+   
 
-  useEffect(() => {
-    setLoading(true);
-    fetch('https://api.github.com/users/Rudresh-sisode')
-    .then((response)=> response.json())
-    .then(setData)
-    .then(() => setLoading(false))
-    .catch(setError)
-  },[]);
 
-  if(loading){
-    return <h1>Loading...</h1>
-  }
-  if(error){
-    return <pre>{JSON.stringify(error,null,2)}</pre>
-  }
-  if(!data){
-    return null;
-  }
-    return <GithubUser name={data.name} />
+
   
 
 }
