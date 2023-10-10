fcac2b8fa85c797ec5b8960ca34aedce88ddb0ec
Rudresh-sisode <rudranrajput@gmail.com>
2023-10-10 10:37:11 +0530

    added the custom hooks

--- ReactJs/react-fun/src/App.js
+++ ReactJs/react-fun/src/App.js
@@ -5,33 +5,55 @@ import { useState, useEffect, useReducer, useRef } from 'react';
 
 const [firstCity, secondCity ] = ["Jamner","Jemuner","Jalgaon"];
 
+function useInput(initialValue){
+  const  [value, setValue] = useState(initialValue);
+  return [
+    {
+      value,
+      onChange:(e) => setValue(e.target.value)
+    },
+    () => setValue(initialValue)
+  ];
 
+}
 
 function App({state}) {
   // const [check, setChecked] = useState(false);
   // const [check, setChecked] = useReducer((checked)=> !checked, false)
 
-  const txtTitle = useRef();
-  const hexColor = useRef();
+  // const txtTitle = useRef();
+  // const hexColor = useRef();
+
+  const [titleProps,resetTitle] = useInput("")
+  const [color,setColor] = useState("#000000");
+
+
 
   const submit = (e) =>{
     e.preventDefault();
-    const title = txtTitle.current.value;
-    const color = txtTitle.current.value;
+    // const title = txtTitle.current.value;
+    // const color = txtTitle.current.value;
 
-    alert(`${title},${color}`);
+    alert(`${titleProps.value},${color}`);
+    // setTitle("");
+    setColor("#000000")
   };
 
   return (
     <div className="App">
       <form onSubmit={submit}>
         <input
-        ref={txtTitle}
+        // ref={txtTitle}
+        value={titleProps.value}
+       
         type='text'
         placeholder='color title...'
         />
         <input
-        ref={hexColor }
+        {...titleProps}
+        // ref={hexColor }
+        value={color}
+        // onChange={(event)=>setColor(event.target.value)}
         type='color'
         />
 
