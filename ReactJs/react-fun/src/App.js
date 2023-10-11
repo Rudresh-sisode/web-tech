992fcce1902522b6520f35eacaea87c980488593
Rudresh-sisode <rudranrajput@gmail.com>
2023-10-12 00:07:40 +0530

    react router training

--- ReactJs/react-fun/src/App.js
+++ ReactJs/react-fun/src/App.js
@@ -1,86 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect, useReducer, useRef } from 'react';
+import { Link,Outlet } from 'react-router-dom';
 
-// const query = `
-// query {
-//   allLifts {
-//     name,
-//     elevationGain,
-//     status
-//   }
-// }
-// `
-
-// const opts = {
-//   method:"POST",
-//   headers:{ "Content-Type":"application/json"},
-//   body:JSON.stringify({query})
-// }
-
-// function GithubUser({name,elevationGain, status}){
-//   return (
-//     <div>
-//       <h2>
-//         {name} is learning React
-//       </h2>
-//       <p>
-//         {elevationGain} {status}
-//       </p>
-//       {/* <img src={} height={150} alt='avatar'/> */}
-//     </div>
-//   )
-// }
-
-const dataLs = [{
-    name:"asl-tera", elevation:45839
-  },
-  {
-    name:"mount-everest",elevation:494884
-  },
-  {
-    name:"Juna-gad", elevation:4599
-  },
-  {
-    name:"Neah-Ghat", elevation:9048
-  }
-  ]
-
-function List({data,renderItem,renderEmpty}){
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {
-        data.map((item)=>(
-          <li key={item.name}>
-            {renderItem(item)}
-          </li>
-        ))
-      }
-    </ul>
+export function Home(){
+  return (
+    <div>
+      <nav>
+        <Link to="/About" > About </Link>
+      </nav>
+      <h2>My websites </h2>
+    </div>
   )
 }
 
-function App({state}) {
- 
-return (
-        <List 
-        data = {dataLs} 
-        renderItem={(item)=>(
-            <>
-            {item.name} - {item.elevation} ft.
-            </>
-          )
-        }
-        renderEmpty={<p>No content available as of now!</p>}
-  />
+export function History(){
+  return (
+    <div>
+      <h4>
+        Our history
+      </h4>
+    </div>
   )
-   
+}
 
+export function About(){
+  return (
+    <div>
+      <h2>About  </h2>
+      <Outlet/>
+    </div>
+  )
+}
 
+export function Contact(){
+  return (
+    <div>
+      <h2>Contact </h2>
+    </div>
+  )
+}
 
-  
+export function App({state}) {
+ 
+return <h1>App</h1>
 
 }
 
