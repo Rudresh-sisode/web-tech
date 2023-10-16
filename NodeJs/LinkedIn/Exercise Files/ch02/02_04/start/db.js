baddac93534c598ed1ee373b7163abcf34e8b6d4
Rudresh-sisode <rudranrajput@gmail.com>
2023-10-16 23:41:28 +0530

    sharding

--- NodeJs/LinkedIn/Exercise Files/ch02/02_04/start/db.js
+++ NodeJs/LinkedIn/Exercise Files/ch02/02_04/start/db.js
@@ -1,10 +1,14 @@
 const { LocalStorage } = require('node-localstorage')
 
-const db = new LocalStorage('data')
+const dbA = new LocalStorage('data-a-m');
+const dbB = new LocalStorage('data-n-z');
 
-const loadCats = () => JSON.parse(db.getItem("cats") || '[]')
 
-const hasCat = name => loadCats()
+const loadCats = db => JSON.parse(db.getItem("cats") || '[]')
+
+const whichDB = name =>  name.match(/^[A-M]|^[a-m]/) ? dbA : dbB;
+
+const hasCat = name => loadCats(whichDB)
     .map(cat => cat.name)
     .includes(name)
 
@@ -12,20 +16,24 @@ module.exports = {
 
     addCat(newCat) {
         if (!hasCat(newCat.name)) {
-            let cats = loadCats()
+            let db = whichDB(newCat.name);
+            let cats = loadCats(db)
             cats.push(newCat)
             db.setItem("cats", JSON.stringify(cats, null, 2))
         }
     },
 
     findCatByName(name) {
-        let cats = loadCats()
+        let db = whichDB(name);
+        let cats = loadCats(db)
         return cats.find(cat => cat.name === name)
     },
 
     findCatsByColor(color) {
-        let cats = loadCats()
-        return cats.filter(cat => cat.color === color)
+        return [
+            ...loadCats(dbA).filter(cat => cat.color === color),
+            ...loadCats(dbB).filter(cat => cat.color === color)
+        ]
     }
 
 }
