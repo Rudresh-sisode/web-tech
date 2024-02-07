1dbb897c25d6da2cd8facc47aad95dcd4b267686
Rudresh-sisode <rudranrajput@gmail.com>
2024-02-07 15:46:33 +0530

    another generic function

--- TypeScript/generics/src/app.ts
+++ TypeScript/generics/src/app.ts
@@ -4,3 +4,18 @@
  }
 
  const mergedObj = merge({a:'bc'},{b:'ac'});
+
+ interface Lenthy{
+    length:number;
+ }
+
+ function countAndDescribe<T extends Lenthy>(element:T):[T,string]{
+    let description = 'Got no value';
+    if(element.length === 1){
+        description = 'Got 1 elements';
+    }
+    else if(element.length > 1 ){
+        description = `Got ${element.length} elements`;
+    }
+    return [element,description];
+ }
