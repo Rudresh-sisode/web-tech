f61972eb7ee4a69370a811c746b33ddefdbab847
Rudresh-sisode <rudranrajput@gmail.com>
2024-02-07 16:02:40 +0530

    generic with keyof

--- TypeScript/generics/src/app.ts
+++ TypeScript/generics/src/app.ts
@@ -19,3 +19,9 @@
     }
     return [element,description];
  }
+
+ function extractAndConvert<T extends object, U extends keyof T>(obj:T,key:U){
+    return 'Value '+ obj[key];
+ }
+
+ extractAndConvert({name:'rudresh'},'name')
