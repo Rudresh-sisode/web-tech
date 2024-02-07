b98b9e543cf573be59a5b875a47c2869a63a1b3f
Rudresh-sisode <rudranrajput@gmail.com>
2024-02-07 12:36:33 +0530

    function overloading

--- TypeScript/2024-leaarning/src/app.ts
+++ TypeScript/2024-leaarning/src/app.ts
@@ -23,6 +23,8 @@ type Admin = {
   
   type Universal = Combinable & Numeric;
   
+  function add(a: string, b: string): string;
+  function add(a: number, b: number): number;
   function add(a: Combinable, b: Combinable) {
     if (typeof a === 'string' || typeof b === 'string') {
       return a.toString() + b.toString();
