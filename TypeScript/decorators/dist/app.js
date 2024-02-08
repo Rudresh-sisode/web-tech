6e622c0719ffd5c147556443f6ce8d3d4a591342
Rudresh-sisode <rudranrajput@gmail.com>
2024-02-08 16:08:01 +0530

    more decorator added

--- TypeScript/decorators/dist/app.js
+++ TypeScript/decorators/dist/app.js
@@ -5,10 +5,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(constructor) {
-    console.log('Logging ');
-    console.log(constructor);
+function Logger(logString) {
+    return function (constructor) {
+        console.log('Logging ' + logString);
+        console.log(constructor);
+    };
 }
+function withTemplate(template, hookId) {
+    return function (_) {
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+            hookEl.innerHTML = template;
+        }
+    };
+}
+// @Logger('Loging - person')
 let Person = class Person {
     constructor() {
         this.name = 'max';
@@ -16,6 +27,17 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger
+    withTemplate('<h1>Hello</h1>', 'app')
 ], Person);
+function Log(target, propertyName) {
+    console.log(target, propertyName);
+}
+class Product {
+    constructor(t) {
+        this.title = t;
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
 //# sourceMappingURL=app.js.map
--- TypeScript/decorators/src/app.ts
+++ TypeScript/decorators/src/app.ts
@@ -6,12 +6,40 @@ function Logger(logString:string){
     }
 }
 
-@Logger('Loging - person')
+function withTemplate(template:string,hookId:string){
+    return function(_:Function){
+        const hookEl = document.getElementById(hookId);
+        if(hookEl){
+            hookEl.innerHTML = template;
+        }
+    }
+}
+
+// @Logger('Loging - person')
+@withTemplate('<h1>Hello</h1>','app')
 class Person{
     name = 'max';
 
     constructor(){
         console.log('Creating the person object');
 
+    }
+}
+
+function Log(target:any,propertyName:string | Symbol){
+    console.log(target,propertyName);
+}
+
+
+//Accesor decorator
+function Log2(target:any, propertyName:string, descriptor:PropertyDescriptor){
+    
+}
+
+class Product{
+    @Log
+    title:string;
+    constructor(t:string){
+        this.title = t;
     }
 }
