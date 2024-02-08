a42e18579dc680c0e21b39336c81fb47a2235370
Rudresh-sisode <rudranrajput@gmail.com>
2024-02-08 12:35:50 +0530

    decorator factory

--- TypeScript/decorators/src/app.ts
+++ TypeScript/decorators/src/app.ts
@@ -1,10 +1,12 @@
  
-function Logger(constructor:Function){
-    console.log('Logging ');
-    console.log(constructor);
+function Logger(logString:string){
+    return function(constructor:Function){
+        console.log('Logging '+logString);
+        console.log(constructor);
+    }
 }
 
-@Logger
+@Logger('Loging - person')
 class Person{
     name = 'max';
 
