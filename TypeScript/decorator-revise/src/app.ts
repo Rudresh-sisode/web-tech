bd9876e871ce7a5a71f8a515fe8f55eeb770c576
Rudresh-sisode <rudranrajput@gmail.com>
2024-04-08 17:30:21 +0530

    added decorator which changes content on html view

--- TypeScript/decorator-revise/src/app.ts
+++ TypeScript/decorator-revise/src/app.ts
@@ -2,13 +2,26 @@ function Logger(logString: string) {
   return function(constructor: Function) {
     console.log('Logging...\t'+logString);
     console.log(constructor);
-
   }
-
 }
-@Logger('person decorator')
+
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template
+      // hookEl.querySelector('h1')?.textContent = p.name;
+      hookEl.querySelector('h1')!.textContent = p.name;
+
+    }
+  }
+}
+
+// @Logger('person decorator')
+@WithTemplate('<h1>hello and welcome to the site</h1>','app')
 class Person{
-  name = 'Max';
+  name = 'Rudresh Sisodiya';
 
   constructor(){
     console.log('Creating person object...',this.name);
