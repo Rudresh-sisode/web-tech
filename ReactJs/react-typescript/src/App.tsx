b06c59145b2f01198d1bbf4d233f1f8531d1545e
Rudresh-sisode <rudranrajput@gmail.com>
2024-05-01 12:03:19 +0530

    component added

---
+++ ReactJs/react-typescript/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import CourseGoal from './components/CourseGoal'
+
+function App() {
+
+
+  return (
+    <main>
+      <CourseGoal/>
+    </main>
+  )
+}
+
+export default App
---
+++ ReactJs/react-typescript/src/components/CourseGoal.tsx
@@ -0,0 +1,9 @@
+export default function CourseGoal() {
+  return <article>
+    <div>
+      <h2>TITLE</h2>
+      <p>DESCRIPTION</p>
+    </div>
+    <button>DELETE</button>
+  </article>
+}
