309f4aaf304dd0f3b931f317f10f40017054f46a
Rudresh-sisode <rudranrajput@gmail.com>
2024-05-16 11:52:02 +0530

    jump game logic solved

---
+++ Javascript/2024/jump-game-2.js
@@ -0,0 +1,16 @@
+let minJumps = (num) => {
+  let n = num.length;
+  let jump = new Array(n).fill(Infinity);
+  jump[0] = 0;
+  for (let i = 0; i < n; i++){
+    for (let j = 1; j <=num[i] && i + i < n; j++){
+      jump[i+j] = Math.min(jump[i+j],jump[i]  + 1)
+    }
+  }
+
+
+  return jump[n - 1];
+}
+
+let nums = [2, 3, 1, 1, 4];
+console.log(minJumps(nums));
