d9a1715860c90d264fffc03479b2f8075f904f59
Rudresh-sisode <rudranrajput@gmail.com>
2024-06-04 15:21:32 +0530

    unique path program added | Dynamic programming

--- Javascript/2014/uniquePath.js
+++ Javascript/2014/uniquePath.js
@@ -1,13 +1,14 @@
 const uniquePath = (m, n) => {
     const dp = Array(m).fill().map(() => Array(n).fill(1));
     
-    for(let i = 1; i < m; i++) {
-        for(let j = 1; j < n; j++) {
+    for(let i = 1; i < m; i++) { // m = 3
+        for(let j = 1; j < n; j++) { // n = 7
             dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            
         }
     }
     
     return dp[m - 1][n - 1];
 }
 
-console.log(uniquePath(3, 2));
+console.log(uniquePath(3, 7));
---
+++ Javascript/2014/uniquePath2.js
@@ -0,0 +1,40 @@
+var uniquePathsWithObstacles = function (obstacleGrid) {
+  let m = obstacleGrid.length;
+  let n = obstacleGrid[0].length;
+  let dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  dp[0][0] = obstacleGrid[0][0] === 1 ? 0 : 1;
+
+  console.log("first ",dp);
+
+  for (let i = 1; i < m; i++) {
+    dp[i][0] = obstacleGrid[i][0] === 1 ? 0 : dp[i - 1][0];
+    console.log(`second dp[${i}][0] `,dp);
+  }
+
+  for (let j = 1; j < n; j++) {
+    dp[0][j] = obstacleGrid[0][j] === 1 ? 0 : dp[0][j - 1];
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      if (obstacleGrid[i][j] === 1) {
+        dp[i][j] = 0;
+      } else {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+  }
+
+  return dp[m - 1][n - 1];
+};
+
+// console.log(uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]])); // Output: 2
+// console.log(uniquePathsWithObstacles([[0,1],[0,0]])); // Output: 1
+// console.log(uniquePathsWithObstacles([[0,0,0,0],[0,0,0,0],[0,0,0,0]])); // Output: 10
+// console.log(uniquePathsWithObstacles([[0,0,0,0],[0,1,0,0],[0,0,0,0]])); // Output: 7
+// console.log(uniquePathsWithObstacles([[0,0,0,0,0],[0,0,1,0,0],[0,0,0,0,0]])); // Output: 7
+// console.log(uniquePathsWithObstacles([[0,0,0,0,0],[0,0,1,0,0],[0,0,0,1,0]])); // Output: 2
+console.log(uniquePathsWithObstacles([[0,0,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,0]])); // Output: 4
+// console.log(uniquePathsWithObstacles([[0,0,0,0,0,0],[0,0,1,0,0,0],[0,0,0,1,0,0],[0,0,0,0,0,0]])); // Output: 7
+// console.log(uniquePathsWithObstacles([[0,0,0,0,0,0],[0,0,1,0,0,0],[0,0,0,1,0,0],[0,0,0,0,1,0]])); // Output: 0
+// console.log(uniquePathsWithObstacles([[0,0,0,0,0,0],[0,0,1,0,0,0],[0,0,0,1,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]])); // Output: 14
