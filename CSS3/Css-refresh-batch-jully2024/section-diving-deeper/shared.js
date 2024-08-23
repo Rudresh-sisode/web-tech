9b881161ae2a499c7a2c64504cccdcc2ecd09ab5
Rudresh-sisode <rudranrajput@gmail.com>
2024-08-23 11:30:55 +0530

    backdrop and model has added the events

--- CSS3/Css-refresh-batch-jully2024/section-diving-deeper/shared.js
+++ CSS3/Css-refresh-batch-jully2024/section-diving-deeper/shared.js
@@ -1,3 +1,32 @@
 var backdrop = document.querySelector('.backdrop');
+var model = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var modelButton = document.querySelector('.modal__action--negative');
+
+for (let i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', function () {
+    backdrop.style.display = 'block';
+    model.style.display = 'block';
+  });
+}
+
+modelButton.addEventListener('click', function () {
+  backdrop.style.display = 'none';
+  model.style.display = 'none';
+});
+
+backdrop.addEventListener('click', function () {
+  backdrop.style.display = 'none';
+  model.style.display = 'none';
+});
+
+
+
+
+
+
+
+
+
 
 console.dir(backdrop);
