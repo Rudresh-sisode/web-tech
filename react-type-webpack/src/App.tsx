53fe92c9c292a5012e113364748bbdf464cd8742
Rudresh-sisode <rudranrajput@gmail.com>
2025-08-22 17:14:42 +0530

    webpack | babel | ts | react

---
+++ react-type-webpack/src/App.tsx
@@ -0,0 +1,3 @@
+export const App = ()=>{
+  return <h1>React Typescript Webpack Starter</h1>
+}
---
+++ react-type-webpack/src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import {App} from "./App";
+const room = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+room.render(<App/>)
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(<App />);
